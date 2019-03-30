19e30770d151eb4cbe5c6964d293bdfa6a641ac5
swain406 <swain406@naver.com>
2019-03-30 14:12:11 +0900

    no message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_1.setOnClickListener{
+            tv_1.text = "버튼을 눌렀습니다"
+        }
     }
 }
